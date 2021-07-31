b9c1cbff71b4a3b7e2c00c02b0dec54faf6c9817
John Doe <fake@fake.fake>
2021-07-31 20:56:11 +0500

    doc: add main description, change comment

--- php/tests/run_all.php
+++ php/tests/run_all.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+    The script runs all the test files with '.test.php' extension
+    which reside in the specified folder
+ */
 // Initialisation
 ini_set('display_errors', 1);
 ini_set('assert.exception', 1);
@@ -8,8 +12,7 @@ if ( $zendAssertion !== '1' ) {
     throw new \Error("Php.ini should set 'zend.assertions' to string '1', but '$zendAssertion' is set");
 }
 
-// Include all '.test.php' files recursively
-$directory = new \RecursiveDirectoryIterator('.');
+// Include all matching files recursively
 $iterator = new \RecursiveIteratorIterator($directory);
 $files = new \RegexIterator($iterator, '/^.+\.test.php$/i', RecursiveRegexIterator::GET_MATCH);
 
