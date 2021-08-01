f70601df1051d2f79f786c88e6c74f68606c42a1
rrramble <ru7ar7@gmail.com>
2021-08-01 11:02:05 +0500

    feature: cover 'isRunning' method

--- php/tests/tasks/Task.test.php
+++ php/tests/tasks/Task.test.php
@@ -13,21 +13,26 @@ $task = new Tasks\Task(Tasks\Task::STATUS_NEW, $customer);
 assert($task->getStatus() === Tasks\Task::STATUS_NEW);
 assert($task->getNextStatus(Tasks\Task::ACTION_CANCEL) === Tasks\Task::STATUS_CANCELLED);
 assert($task->getNextStatus(Tasks\Task::ACTION_APPLY) === Tasks\Task::STATUS_RUNNING);
+assert(! $task->isRunning());
 
 // Testing of 'CANCELLED' status
 $task = new Tasks\Task(Tasks\Task::STATUS_CANCELLED, $customer, $contractor);
 assert($task->getStatus() === Tasks\Task::STATUS_CANCELLED);
+assert(! $task->isRunning());
 
 // Testing of 'RUNNING' status
 $task = new Tasks\Task(Tasks\Task::STATUS_RUNNING, $customer, $contractor);
 assert($task->getStatus() === Tasks\Task::STATUS_RUNNING);
+assert($task->isRunning());
 assert($task->getNextStatus(Tasks\Task::ACTION_CONFIRM_DONE) === Tasks\Task::STATUS_DONE);
 assert($task->getNextStatus(Tasks\Task::ACTION_REJECT) === Tasks\Task::STATUS_FAILED);
 
 // Testing of 'DONE' status
 $task = new Tasks\Task(Tasks\Task::STATUS_DONE, $customer, $contractor);
 assert($task->getStatus() === Tasks\Task::STATUS_DONE);
+assert(! $task->isRunning());
 
 // Testing of 'FAILED' status
 $task = new Tasks\Task(Tasks\Task::STATUS_FAILED, $customer, $contractor);
 assert($task->getStatus() === Tasks\Task::STATUS_FAILED);
+assert(! $task->isRunning());
