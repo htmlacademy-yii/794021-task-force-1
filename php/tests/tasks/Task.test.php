6f1faab9a0ee8b8469472e33d883efb36c166b4a
rrramble <ru7ar7@gmail.com>
2021-08-01 11:02:44 +0500

    feature: cover 'getCustomer', 'getContractor' methods

--- php/tests/tasks/Task.test.php
+++ php/tests/tasks/Task.test.php
@@ -14,11 +14,16 @@ assert($task->getStatus() === Tasks\Task::STATUS_NEW);
 assert($task->getNextStatus(Tasks\Task::ACTION_CANCEL) === Tasks\Task::STATUS_CANCELLED);
 assert($task->getNextStatus(Tasks\Task::ACTION_APPLY) === Tasks\Task::STATUS_RUNNING);
 assert(! $task->isRunning());
+assert($task->getCustomer() == $customer);
+assert(! $task->getContractor());
 
 // Testing of 'CANCELLED' status
 $task = new Tasks\Task(Tasks\Task::STATUS_CANCELLED, $customer, $contractor);
 assert($task->getStatus() === Tasks\Task::STATUS_CANCELLED);
 assert(! $task->isRunning());
+assert($task->getCustomer() == $customer);
+assert($task->getContractor() == $contractor);
+assert($task->getCustomer() !== $task->getContractor());
 
 // Testing of 'RUNNING' status
 $task = new Tasks\Task(Tasks\Task::STATUS_RUNNING, $customer, $contractor);
@@ -26,13 +31,22 @@ assert($task->getStatus() === Tasks\Task::STATUS_RUNNING);
 assert($task->isRunning());
 assert($task->getNextStatus(Tasks\Task::ACTION_CONFIRM_DONE) === Tasks\Task::STATUS_DONE);
 assert($task->getNextStatus(Tasks\Task::ACTION_REJECT) === Tasks\Task::STATUS_FAILED);
+assert($task->getCustomer() == $customer);
+assert($task->getContractor() == $contractor);
+assert($task->getCustomer() !== $task->getContractor());
 
 // Testing of 'DONE' status
 $task = new Tasks\Task(Tasks\Task::STATUS_DONE, $customer, $contractor);
 assert($task->getStatus() === Tasks\Task::STATUS_DONE);
 assert(! $task->isRunning());
+assert($task->getCustomer() == $customer);
+assert($task->getContractor() == $contractor);
+assert($task->getCustomer() !== $task->getContractor());
 
 // Testing of 'FAILED' status
 $task = new Tasks\Task(Tasks\Task::STATUS_FAILED, $customer, $contractor);
 assert($task->getStatus() === Tasks\Task::STATUS_FAILED);
 assert(! $task->isRunning());
+assert($task->getCustomer() == $customer);
+assert($task->getContractor() == $contractor);
+assert($task->getCustomer() !== $task->getContractor());
