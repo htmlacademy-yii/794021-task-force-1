1019fa9454e34da34b4817ee8c20ea029ac106d8
rrramble <ru7ar7@gmail.com>
2021-08-03 21:35:58 +0500

    refactor: import and use concrete class instead of partial namespace

--- src/tests/actions/Cancel.test.php
+++ src/tests/actions/Cancel.test.php
@@ -1,16 +1,17 @@
 <?php
 
-use R794021\Actions;
-use R794021\Tasks;
-use R794021\Users;
+use R794021\Actions\Cancel;
+use R794021\Tasks\Task;
+use R794021\Users\Contractor;
+use R794021\Users\Customer;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-$cancel = new Actions\Cancel();
-$customer1 = new Users\Customer(CUSTOMER_1);
-$contractor1 = new Users\Contractor(CONTRACTOR_1);
-$customer2 = new Users\Customer(CUSTOMER_2);
-$contractor2 = new Users\Contractor(CONTRACTOR_2);
+$cancel = new Cancel();
+$customer1 = new Customer(CUSTOMER_1);
+$contractor1 = new Contractor(CONTRACTOR_1);
+$customer2 = new Customer(CUSTOMER_2);
+$contractor2 = new Contractor(CONTRACTOR_2);
 
 /*
     Checking the name of the Action
@@ -31,7 +32,7 @@ assert($cancel->getInternalCodename() !== 'apply');
     Expected:
         Only its customer can dispatch 'Cancel' action
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_NEW, $customer1);
+$task1 = new Task(Task::STATUS_NEW, $customer1);
 assert($cancel->isValid($customer1, $task1));
 assert(! $cancel->isValid($customer2, $task1));
 assert(! $cancel->isValid($contractor1, $task1));
@@ -44,7 +45,7 @@ assert(! $cancel->isValid($contractor2, $task1));
     Expected:
         No user can cancel the task
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_RUNNING, $customer1, $contractor1);
+$task1 = new Task(Task::STATUS_RUNNING, $customer1, $contractor1);
 assert(! $cancel->isValid($customer1, $task1));
 assert(! $cancel->isValid($customer2, $task1));
 assert(! $cancel->isValid($contractor1, $task1));
@@ -57,7 +58,7 @@ assert(! $cancel->isValid($contractor2, $task1));
     Expected:
         No one can cancel the task
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_CANCELLED, $customer1, $contractor1);
+$task1 = new Task(Task::STATUS_CANCELLED, $customer1, $contractor1);
 assert(! $cancel->isValid($customer1, $task1));
 assert(! $cancel->isValid($customer2, $task1));
 assert(! $cancel->isValid($contractor1, $task1));
@@ -70,7 +71,7 @@ assert(! $cancel->isValid($contractor2, $task1));
     Expected:
         No one can cancel the task the second time
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_DONE, $customer1, $contractor1);
+$task1 = new Task(Task::STATUS_DONE, $customer1, $contractor1);
 assert(! $cancel->isValid($customer1, $task1));
 assert(! $cancel->isValid($customer2, $task1));
 assert(! $cancel->isValid($contractor1, $task1));
@@ -83,7 +84,7 @@ assert(! $cancel->isValid($contractor2, $task1));
     Expected:
         No one can cancel the task the second time
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_DONE, $customer1, $contractor1);
+$task1 = new Task(Task::STATUS_DONE, $customer1, $contractor1);
 assert(! $cancel->isValid($customer1, $task1));
 assert(! $cancel->isValid($customer2, $task1));
 assert(! $cancel->isValid($contractor1, $task1));
--- src/tests/actions/Done.test.php
+++ src/tests/actions/Done.test.php
@@ -1,16 +1,17 @@
 <?php
 
-use R794021\Actions;
-use R794021\Tasks;
-use R794021\Users;
+use R794021\Actions\Done;
+use R794021\Tasks\Task;
+use R794021\Users\Contractor;
+use R794021\Users\Customer;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-$done = new Actions\Done();
-$customer1 = new Users\Customer(CUSTOMER_1);
-$customer2 = new Users\Customer(CUSTOMER_2);
-$contractor1 = new Users\Contractor(CONTRACTOR_1);
-$contractor2 = new Users\Contractor(CONTRACTOR_2);
+$done = new Done();
+$customer1 = new Customer(CUSTOMER_1);
+$customer2 = new Customer(CUSTOMER_2);
+$contractor1 = new Contractor(CONTRACTOR_1);
+$contractor2 = new Contractor(CONTRACTOR_2);
 
 /*
     Checking the name of the Action
@@ -31,7 +32,7 @@ assert($done->getInternalCodename() !== ' done');
     Expected:
         No one may confirm the task is done
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_NEW, $customer1);
+$task1 = new Task(Task::STATUS_NEW, $customer1);
 assert(! $done->isValid($customer1, $task1));
 assert(! $done->isValid($customer2, $task1));
 assert(! $done->isValid($contractor1, $task1));
@@ -44,7 +45,7 @@ assert(! $done->isValid($contractor2, $task1));
     Expected:
         Only its customer may confirm the task is done
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_RUNNING, $customer1, $contractor1);
+$task1 = new Task(Task::STATUS_RUNNING, $customer1, $contractor1);
 assert($done->isValid($customer1, $task1));
 assert(! $done->isValid($customer2, $task1));
 assert(! $done->isValid($contractor1, $task1));
@@ -57,7 +58,7 @@ assert(! $done->isValid($contractor2, $task1));
     Expected:
         Only its customer may confirm the task is done
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_FAILED, $customer1, $contractor1);
+$task1 = new Task(Task::STATUS_FAILED, $customer1, $contractor1);
 assert(! $done->isValid($customer1, $task1));
 assert(! $done->isValid($customer2, $task1));
 assert(! $done->isValid($contractor1, $task1));
@@ -70,7 +71,7 @@ assert(! $done->isValid($contractor2, $task1));
     Expected:
         No one may re-confirm the task is done
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_DONE, $customer1, $contractor1);
+$task1 = new Task(Task::STATUS_DONE, $customer1, $contractor1);
 assert(! $done->isValid($customer1, $task1));
 assert(! $done->isValid($customer2, $task1));
 assert(! $done->isValid($contractor1, $task1));
