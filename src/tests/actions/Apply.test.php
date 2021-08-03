670757ecfc57bc5602a5416a4bd2c81d828233ab
rrramble <ru7ar7@gmail.com>
2021-08-03 21:18:11 +0500

    refactor: use common constants from 'init.php'

--- src/tests/actions/Apply.test.php
+++ src/tests/actions/Apply.test.php
@@ -7,11 +7,6 @@ use R794021\Users\Contractor;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-const CUSTOMER_1 = ['id' => 12];
-const CUSTOMER_2 = ['id' => 13];
-const CONTRACTOR_1 = ['id' => 24];
-const CONTRACTOR_2 = ['id' => 26];
-
 $apply = new Apply();
 $customer1 = new Customer(CUSTOMER_1);
 $customer2 = new Customer(CUSTOMER_2);
