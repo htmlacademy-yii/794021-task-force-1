9b328cd4da58599a2b4c38d840beff3640345f7a
rrramble <ru7ar7@gmail.com>
2021-08-03 21:29:18 +0500

    refactor: import and use concrete classes, instead of parent namespace

--- src/tests/actions/Reject.test.php
+++ src/tests/actions/Reject.test.php
@@ -1,16 +1,17 @@
 <?php
 
-use R794021\Actions;
-use R794021\Tasks;
-use R794021\Users;
+use R794021\Actions\Reject;
+use R794021\Tasks\Task;
+use R794021\Users\Contractor;
+use R794021\Users\Customer;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-$reject = new Actions\Reject();
-$customer1 = new Users\Customer(CUSTOMER_1);
-$customer2 = new Users\Customer(CUSTOMER_2);
-$contractor1 = new Users\Contractor(CONTRACTOR_1);
-$contractor2 = new Users\Contractor(CONTRACTOR_2);
+$reject = new Reject();
+$customer1 = new Customer(CUSTOMER_1);
+$customer2 = new Customer(CUSTOMER_2);
+$contractor1 = new Contractor(CONTRACTOR_1);
+$contractor2 = new Contractor(CONTRACTOR_2);
 
 /*
     Checking the name of the Action
@@ -31,7 +32,7 @@ assert($reject->getInternalCodename() !== ' reject');
     Expected:
         No one may reject unstarted task
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_NEW, $customer1);
+$task1 = new Task(Task::STATUS_NEW, $customer1);
 assert(! $reject->isValid($customer1, $task1));
 assert(! $reject->isValid($customer2, $task1));
 assert(! $reject->isValid($contractor1, $task1));
@@ -44,7 +45,7 @@ assert(! $reject->isValid($contractor2, $task1));
     Expected:
         Only its contractor may reject the running task
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_RUNNING, $customer1, $contractor1);
+$task1 = new Task(Task::STATUS_RUNNING, $customer1, $contractor1);
 assert($reject->isValid($contractor1, $task1));
 assert(! $reject->isValid($contractor2, $task1));
 assert(! $reject->isValid($customer1, $task1));
@@ -57,7 +58,7 @@ assert(! $reject->isValid($customer2, $task1));
     Expected:
         No one may reject the finished task
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_DONE, $customer1);
+$task1 = new Task(Task::STATUS_DONE, $customer1);
 assert(! $reject->isValid($contractor1, $task1));
 assert(! $reject->isValid($contractor2, $task1));
 assert(! $reject->isValid($customer1, $task1));
@@ -70,7 +71,7 @@ assert(! $reject->isValid($customer2, $task1));
     Expected:
         No one may reject the Failed task
 */
-$task1 = new Tasks\Task(Tasks\Task::STATUS_FAILED, $customer1);
+$task1 = new Task(Task::STATUS_FAILED, $customer1);
 assert(! $reject->isValid($contractor1, $task1));
 assert(! $reject->isValid($contractor2, $task1));
 assert(! $reject->isValid($customer1, $task1));
