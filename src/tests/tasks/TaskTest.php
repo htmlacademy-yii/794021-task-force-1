84fb5f0d88cf51586d7c08e608b428af50b7f60a
rrramble <ru7ar7@gmail.com>
2021-08-03 22:44:10 +0500

    refactor: import and user concrete classes, instead of part of the namespaces

--- src/tests/tasks/TaskTest.php
+++ src/tests/tasks/TaskTest.php
@@ -1,49 +1,50 @@
 <?php
 
-use R794021\Tasks;
-use R794021\Users;
+use R794021\Tasks\Task;
+use R794021\Users\Contractor;
+use R794021\Users\Customer;
 
-$customer = new Users\Customer(CUSTOMER_1);
-$contractor = new Users\Contractor(CONTRACTOR_1);
+$customer = new Customer(CUSTOMER_1);
+$contractor = new Contractor(CONTRACTOR_1);
 
 // Testing of 'NEW' status
-$task = new Tasks\Task(Tasks\Task::STATUS_NEW, $customer);
-assert($task->getStatus() === Tasks\Task::STATUS_NEW);
-assert($task->getNextStatus(Tasks\Task::ACTION_CANCEL) === Tasks\Task::STATUS_CANCELLED);
-assert($task->getNextStatus(Tasks\Task::ACTION_APPLY) === Tasks\Task::STATUS_RUNNING);
+$task = new Task(Task::STATUS_NEW, $customer);
+assert($task->getStatus() === Task::STATUS_NEW);
+assert($task->getNextStatus(Task::ACTION_CANCEL) === Task::STATUS_CANCELLED);
+assert($task->getNextStatus(Task::ACTION_APPLY) === Task::STATUS_RUNNING);
 assert(! $task->isRunning());
 assert($task->getCustomer() == $customer);
 assert(! $task->getContractor());
 
 // Testing of 'CANCELLED' status
-$task = new Tasks\Task(Tasks\Task::STATUS_CANCELLED, $customer, $contractor);
-assert($task->getStatus() === Tasks\Task::STATUS_CANCELLED);
+$task = new Task(Task::STATUS_CANCELLED, $customer, $contractor);
+assert($task->getStatus() === Task::STATUS_CANCELLED);
 assert(! $task->isRunning());
 assert($task->getCustomer() == $customer);
 assert($task->getContractor() == $contractor);
 assert($task->getCustomer() !== $task->getContractor());
 
 // Testing of 'RUNNING' status
-$task = new Tasks\Task(Tasks\Task::STATUS_RUNNING, $customer, $contractor);
-assert($task->getStatus() === Tasks\Task::STATUS_RUNNING);
+$task = new Task(Task::STATUS_RUNNING, $customer, $contractor);
+assert($task->getStatus() === Task::STATUS_RUNNING);
 assert($task->isRunning());
-assert($task->getNextStatus(Tasks\Task::ACTION_CONFIRM_DONE) === Tasks\Task::STATUS_DONE);
-assert($task->getNextStatus(Tasks\Task::ACTION_REJECT) === Tasks\Task::STATUS_FAILED);
+assert($task->getNextStatus(Task::ACTION_CONFIRM_DONE) === Task::STATUS_DONE);
+assert($task->getNextStatus(Task::ACTION_REJECT) === Task::STATUS_FAILED);
 assert($task->getCustomer() == $customer);
 assert($task->getContractor() == $contractor);
 assert($task->getCustomer() !== $task->getContractor());
 
 // Testing of 'DONE' status
-$task = new Tasks\Task(Tasks\Task::STATUS_DONE, $customer, $contractor);
-assert($task->getStatus() === Tasks\Task::STATUS_DONE);
+$task = new Task(Task::STATUS_DONE, $customer, $contractor);
+assert($task->getStatus() === Task::STATUS_DONE);
 assert(! $task->isRunning());
 assert($task->getCustomer() == $customer);
 assert($task->getContractor() == $contractor);
 assert($task->getCustomer() !== $task->getContractor());
 
 // Testing of 'FAILED' status
-$task = new Tasks\Task(Tasks\Task::STATUS_FAILED, $customer, $contractor);
-assert($task->getStatus() === Tasks\Task::STATUS_FAILED);
+$task = new Task(Task::STATUS_FAILED, $customer, $contractor);
+assert($task->getStatus() === Task::STATUS_FAILED);
 assert(! $task->isRunning());
 assert($task->getCustomer() == $customer);
 assert($task->getContractor() == $contractor);
