9e2a1a7def2361ec6702c5f8442e920f9833aa3d
rrramble <ru7ar7@gmail.com>
2021-08-04 00:04:24 +0500

    fix: check is Contractor and Customer are instance of User

--- src/tests/users/CustomerTest.php
+++ src/tests/users/CustomerTest.php
@@ -1,6 +1,7 @@
 <?php
-use R794021\Users\Customer;
+use R794021\Users\{Customer, User};
 
 $customer = new Customer(CUSTOMER_1);
+assert($customer instanceof User);
 assert($customer->isCustomer());
 assert(! $customer->isContractor());
