53178aafc41b0de915d7814bb09ee6e57877e3a1
rrramble <ru7ar7@gmail.com>
2021-08-04 21:50:40 +0500

    refactor: use 'DataDomainException' when check for user 'id' field existance

--- src/user/AbstractUser.php
+++ src/user/AbstractUser.php
@@ -2,17 +2,22 @@
 
 namespace R794021\User;
 
+use R794021\Exception\DataDomainException;
+
 
 abstract class AbstractUser
 {
     protected $id;
 
-    public function __construct($info)
+    public function __construct(array $userInfo)
     {
-        if ( ! isset($info) && array_key_exists('id', $info) ) {
-            throw new \DomainException('User id is not set');
+        if ( ! array_key_exists('id', $userInfo) ) {
+            throw new DataDomainException('User id field should exist');
         }
-        $this->id = $info['id'];
+        $this->id = $userInfo['id'];
+        if ( ! is_int($this->id) || $this->id <= 0 ) {
+            throw new DataDomainException('User id should be positive integer');
+        }
     }
 
     public function getId(): int
