6e95f8bf86b2fb0ca3f4ec79f42d3471f4e7cee6
rrramble <ru7ar7@gmail.com>
2021-08-04 23:39:02 +0500

    fix: throw exception on incorrect parameters

--- test/init.php
+++ test/init.php
@@ -5,5 +5,5 @@ const CUSTOMER_2 = ['id' => 13];
 const CONTRACTOR_1 = ['id' => 24];
 const CONTRACTOR_2 = ['id' => 26];
 
-const USER_NO_ID_FIELD = ['idd' => 1];
+const USER_HAS_NO_ID_FIELD = ['idd' => 1];
 const USER_ID_FIELD_NOT_POSITIVE_INTEGER = ['id' => '-1.0'];
--- test/user/AbstractUserTest.php
+++ test/user/AbstractUserTest.php
@@ -2,31 +2,37 @@
 use R794021\User\{AbstractUser, User};
 use R794021\Exception\DataDomainException;
 
-const USER_NO_ID_FIELD_EXCEPTION_TEXT = 'User id field should exist';
+const USER_HAS_NO_ID_FIELD_EXCEPTION_TEXT = 'User id field should exist';
 const USER_ID_FIELD_NOT_POSITIVE_INTEGER_EXCEPTION_TEXT =
     'User id should be positive integer';
 
 
 // Test if 'id' field exist in user data
 try {
-    $user = new class (USER_NO_ID_FIELD) extends AbstractUser implements User {
+    $hasException = false;
+    $user = new class (USER_HAS_NO_ID_FIELD) extends AbstractUser implements User {
         public static function isContractor(): bool { return false; }
         public static function isCustomer(): bool { return false; }
 
     };
 } catch (DataDomainException $e) {
-    assert($e->getMessage() === USER_NO_ID_FIELD_EXCEPTION_TEXT);
+    $hasException = true;
+    assert($e->getMessage() === USER_HAS_NO_ID_FIELD_EXCEPTION_TEXT);
+} finally {
+    assert($hasException, 'There must be exception!');
 }
 
 // Test if 'id' field is a positive integer
 try {
-    $user = new class (USER_ID_FIELD_NOT_POSITIVE_INTEGER)
-        extends AbstractUser implements User
-    {
+    $hasException = false;
+    $user = new class (USER_ID_FIELD_NOT_POSITIVE_INTEGER) extends AbstractUser implements User {
         public static function isContractor(): bool { return false; }
         public static function isCustomer(): bool { return false; }
 
     };
 } catch (DataDomainException $e) {
+    $hasException = true;
     assert($e->getMessage() === USER_ID_FIELD_NOT_POSITIVE_INTEGER_EXCEPTION_TEXT);
+} finally {
+    assert($hasException, 'There must be exception!');
 }
