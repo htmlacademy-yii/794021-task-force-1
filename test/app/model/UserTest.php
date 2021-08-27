33123a88fdcfbcbfa0b68700d0987277a4813ec4
rrramble <ru7ar7@gmail.com>
2021-08-28 00:38:42 +0500

    featre: add test for 'User' ActiveRecord model

---
+++ test/app/model/UserTest.php
@@ -0,0 +1,12 @@
+<?php
+use frontend\models\User;
+
+$user = new User();
+
+assert($user->tableName() === 'user');
+
+$rules = $user->rules();
+assert(is_array($rules));
+
+$attributeLabels = $user->attributeLabels();
+assert(is_array($attributeLabels));
