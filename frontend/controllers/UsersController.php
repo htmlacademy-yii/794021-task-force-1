d4d4df6d5215021fcf3b33a1e4a9fa17e9835af7
rrramble <ru7ar7@gmail.com>
2021-09-25 22:31:38 +0500

    feature: add UsersController

--- frontend/controllers/UsersController.php
+++ frontend/controllers/UsersController.php
@@ -3,24 +3,39 @@
 namespace frontend\controllers;
 use yii\web\Controller;
 use \yii\db\ActiveQuery;
+use \yii\db\Query;
+use frontend\models\Contractor;
+use frontend\models\ContractorOccupation;
 use frontend\models\User;
+use frontend\models\Task;
 
 class UsersController extends Controller
 {
     public function actionIndex()
     {
-        // TODO: show count of the tasks being made
-        // TODO: show reviews count
-        // TODO: show rating
-        // TODO: show stars, based on rating
-        // TODO: define proper sorting: by user registration, or last activity
+        $contractors = (new Query())->select('contractor_id')->from('contractor_occupation')->groupBy('contractor_id')->column();
+
         $users = User::find()
-            ->joinWith([
-                'contractorOccupations' => function (ActiveQuery $query) {
-                    $query->where('contractor_id');
-                }])
-            ->orderBy('datetime_created', 'desc')
-            ->all();
+        ->with('contractorOccupations')
+        ->with('tasks.review')
+        ->select([
+            '`user`.`id`',
+            '`user`.`fullname`',
+            '`user`.`website_last_action_datetime`',
+            'COUNT(`task`.`contractor_id`) AS `doneTaskCount`',
+            'COUNT(`review`.`task_id`) AS `reviewCount`',
+            'AVG(`review`.`rating`) AS `rating`'
+        ])
+        ->leftJoin('task', 'task.contractor_id = user.id')
+        ->leftJoin('review', 'task.id = review.task_id')
+
+        ->andWhere(['task.state_id' => Task::STATE_DONE])
+        ->andWhere(['user.id' => $contractors])
+        ->andWhere(['user.hide_profile' => false])
+        ->groupBy('id')
+        ->orderBy('user.datetime_created ASC')
+        ->all()
+        ;
         return $this->render('index', ['items' => $users]);
     }
 }
